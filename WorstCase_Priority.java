f1f41ed07d25796aa1bd1b2599ecc8b2f4eeabe9
aoba <65536toaru@gmail.com>
2020-09-24 05:13:32 +0900

    new file:   .gitignore 	new file:   GenTestCase.java 	new file:   WorstCase_Priority.java

---
+++ WorstCase_Priority.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+class WorstCase_Priority{
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int process = 10000;
+		System.out.println(process);
+		int quantum = rand.nextInt(10) + 1;
+		System.out.println(quantum);
+		for(int n = 0;n < process;n++) {
+			int arrival = rand.nextInt(process * 10);
+			int deadline = rand.nextInt(process * 25) + 1;
+			int remaining = rand.nextInt(10) + 1;
+			System.out.println(arrival);
+			System.out.println(deadline);
+			System.out.println(remaining);
+			System.out.println(remaining);
+		}
+	}
+}
