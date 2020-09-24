cbc67591d1064d7308931c20fc5bcec2b3ff38bc
aoba <65536toaru@gmail.com>
2020-09-24 09:50:25 +0900

    new file:   BestCase_Priority.java 	renamed:    DefaultTestCase.txt -> situation&result/default/DefaultTestCase.txt 	renamed:    DefaultTestCase_Result.txt -> situation&result/default/DefaultTestCase_Result.txt 	renamed:    WorstCase_Priority.txt -> situation&result/priority/WorstCase/WorstCase_Priority.txt 	renamed:    WorstCase_Priority_Result.txt -> situation&result/priority/WorstCase/WorstCase_Priority_Result.txt

---
+++ BestCase_Priority.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+class BestCase_Priority{
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int process = 10000;
+		System.out.println(process);
+		int quantum = rand.nextInt(10) + 1;
+		System.out.println(quantum);
+		for(int n = 0;n < process;n++) {
+			int arrival = rand.nextInt(process * 10);
+			int deadline = rand.nextInt(process * 25) + 1;
+			int priority = rand.nextInt(10000);
+			int remaining = rand.nextInt(10) + 1;
+			System.out.println(arrival);
+			System.out.println(deadline);
+			System.out.println(priority);
+			System.out.println(-remaining);
+		}
+	}
+}
