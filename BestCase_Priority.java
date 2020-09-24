43e219873e013bd500ed62d7505697d46504b699
aoba <65536toaru@gmail.com>
2020-09-24 10:17:54 +0900

    modified:   BestCase_Priority.java

--- BestCase_Priority.java
+++ BestCase_Priority.java
@@ -10,7 +10,6 @@ class BestCase_Priority{
 		for(int n = 0;n < process;n++) {
 			int arrival = rand.nextInt(process * 10);
 			int deadline = rand.nextInt(process * 25) + 1;
-			int priority = rand.nextInt(10000);
 			int remaining = rand.nextInt(10) + 1;
 			System.out.println(arrival);
 			System.out.println(deadline);
